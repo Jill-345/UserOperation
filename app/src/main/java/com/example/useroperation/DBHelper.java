c9cc79df85c4f3cd464c4a75e0b2e0657d6c6289
Cristel-J <cristelantonettejimenez@gmail.com>
2025-10-06 16:57:24 +0800

    dbhelper(almost done)

--- app/src/main/java/com/example/useroperation/DBHelper.java
+++ app/src/main/java/com/example/useroperation/DBHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper{
@@ -13,7 +14,7 @@ public class DBHelper extends SQLiteOpenHelper{
 
     private final static String tblname = "users";
     private final static String column1 = "UID";
-    private final static String column2 = "UserN";
+    private final static String column2 = "UN";
     private final static String column3 = "FN";
     private final static String column4 = "LN";
     private final static String column5 = "Pass";
@@ -42,6 +43,69 @@ public class DBHelper extends SQLiteOpenHelper{
         onCreate(sqLiteDatabase);
     }
 
+    public Cursor getAllRecords(){
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "select * from " + tblname + " where deleted='NO'";
+        return db.rawQuery(query, null);
+    }
 
+    public Cursor getRecord(String uid){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "select * from " + tblname + " where uid='" + uid + "'";
+        return db.rawQuery(query, null);
+    }
+
+    public String getUID(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("select uid from " + tblname, null);;
+        int count = cursor.getCount();
+        String uid = "000000";
+        if(count >= 0 && count < 9){
+            uid = "00000" + (count+1);
+        }
+        else if(count >= 9 && count < 99){
+            uid = "0000" + (count+1);
+        }
+        else if(count >= 99 && count < 999){
+            uid = "000" + (count+1);
+        }
+        else if(count >= 999 && count < 9999){
+            uid = "00" + (count+1);
+        }
+        else if(count >= 9999 && count < 99999){
+            uid = "0" + (count+1);
+        }
+        else if(count >= 99999 && count < 999999){
+            uid = "" + (count+1);
+
+        }
+        return uid;
+    }
+
+    public void addNewRecord(String un,String fn, String ln, String course, String pass){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(column1,getUID());
+        values.put(column2, un);
+        values.put(column3, fn);
+        values.put(column4, ln);
+        values.put(column5, pass);
+        values.put(column6, "NO");
+        db.insert(tblname, null, values);
+        db.close();
+    }
+
+    public void editRecord(String un,String fn, String ln, String course, String pass){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(column1,getUID());
+        values.put(column2, un);
+        values.put(column3, fn);
+        values.put(column4, ln);
+        values.put(column5, pass);
+        values.put(column6, "NO");
+        db.update(tblname, values,"sid=?",args);
+        db.close();
+    }
 
 }
