ede6412ca79d8f234e052ed04c119199fb5465d1
Jilliane Jaime <jillianejaime09@gmail.com>
2025-10-07 20:14:19 +0800

    clear btn in main act, added sample only java and xml

--- app/src/main/java/com/example/useroperation/sampleonly.java
+++ app/src/main/java/com/example/useroperation/sampleonly.java
@@ -1,8 +1,6 @@
 package com.example.useroperation;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    EditText unTxt, passTxt;
-    Button logInBtn, regisBtn, clearBtn;
+public class sampleonly extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sampleonly);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        unTxt = findViewById(R.id.editTextText2);
-        passTxt = findViewById(R.id.editTextText8);
-        logInBtn = findViewById(R.id.button);
-        regisBtn = findViewById(R.id.button2);
-        clearBtn = findViewById(R.id.button3);
     }
 }
